06e1f2d07b76c426a10b0b43dd02380b706eb3c9
Francisco Arencibia Quesada <arencibia.francisco@gmail.com>
2024-12-31 00:14:50 +0100

    Update vite.config.js
    Signed-off-by: Francisco Arencibia Quesada <arencibia.francisco@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',  // Asegúrate de reemplazar 'xopsmainpage-react' con el nombre correcto de tu repositorio
+  base: '/xopsmainpage-react/',  // Asegúrate de reemplazar 'xopsmainpage-react' con el nombre correcto de tu repositorio
   plugins: [react()],
   optimizeDeps: {
     include: [
