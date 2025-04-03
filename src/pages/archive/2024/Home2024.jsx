b82771d5844dd5a83427f45787219149a561da4d
laurajimu <laura.jimenez@hackerdreams.org>
2025-04-03 14:59:18 +0000

    archivo de home y organizer 2024

---
+++ src/pages/archive/2024/Home2024.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import XOpsSection from "../components/XOpsSection";
+import Themes from "../components/Themes";
+import SpeakersSection from "../components/SpeakersSection";
+import Events from "../components/Events/Events";
+import Ubication from "../components/Ubication";
+import Collaborators from "../components/Collaborators";
+
+
+const Home = () => {
+
+  return (
+    <>
+
+      <XOpsSection />
+      <Themes />
+      <Events />
+      <Ubication />
+      <Collaborators />
+      <SpeakersSection />
+
+    </>
+  );
+};
+
+export default Home;
