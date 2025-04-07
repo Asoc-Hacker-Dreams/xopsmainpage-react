86e07e4d35ceaa45e3503b3a8823b6e6fff8c871
laurajimu <laura.jimenez@hackerdreams.org>
2025-04-07 11:33:56 +0000

    home

--- src/pages/archive/2024/Home2024.jsx
+++ src/pages/archive/2024/Home2024.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import XOpsSection from "../components/XOpsSection";
 import Themes from "../components/Themes";
 import SpeakersSection from "../components/SpeakersSection";
 import Events from "../components/Events/Events";
 import Ubication from "../components/Ubication";
 import Collaborators from "../components/Collaborators";
+import XOpsSection from '@components/XOpsSection';
 
 
 
