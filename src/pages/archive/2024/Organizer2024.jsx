2e9b50883cb21832bc1638e690448322d25d3dda
laurajimu <laura.jimenez@hackerdreams.org>
2025-04-07 11:42:49 +0000

    añado ruta import organizers

--- src/pages/archive/2024/Organizer2024.jsx
+++ src/pages/archive/2024/Organizer2024.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import Organizers from "../components/Organizers";
+import Organizers from "../../components/Organizers";
 import { useLocation } from 'react-router-dom';
 
 
