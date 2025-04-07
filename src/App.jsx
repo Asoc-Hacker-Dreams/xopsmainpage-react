6eb892afc0405c14e797ca43d6473e78c43ab150
laurajimu <laura.jimenez@hackerdreams.org>
2025-04-07 07:43:58 +0000

    quito todo 2024

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
                 <Link className='links px-4 font-weight-bold text-white' to="/#ponentes" style={{ marginTop: '10px', marginBottom: '10px', textDecoration: 'none' }}>PONENTES</Link>
                 <Link className='links px-4 font-weight-bold text-white' to="/Sponsor#patrocinio"  style={{ marginTop: '10px', marginBottom: '10px', textDecoration: 'none' }}>CONVIERTETE EN PATROCINADOR</Link>
                 <Link className='links px-4 font-weight-bold text-white' to="/Organizer#organizr" style={{ marginTop: '10px', marginBottom: '10px', textDecoration: 'none' }}>ORGANIZADORES</Link>
-                <Link className='links px-4 font-weight-bold text-white' to="/Organizer2024#Organizr" style={{ marginTop: '10px', marginBottom: '10px', textDecoration: 'none' }}>EVENTOS ANTERIORES</Link>                                                   '
+                <Link className='links px-4 font-weight-bold text-white' to="/Organizer#Organizr" style={{ marginTop: '10px', marginBottom: '10px', textDecoration: 'none' }}>EVENTOS ANTERIORES</Link>                                                   '
             </Nav>
         <a href="https://www.eventbrite.es/e/entradas-xops-conference-1049115200807?aff=oddtdtcreator" className="button menu-btn" style={{ textDecoration: 'none' }}>
             TICKETS
@@ -61,7 +61,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Organizer" element={<Organizer />} />
         <Route path="/Sponsor" element={<Sponsor />} />    
-        <Route path="/Organizer2024" element={<Organizer2024 />} />    
+           
       </Routes>
 
 
