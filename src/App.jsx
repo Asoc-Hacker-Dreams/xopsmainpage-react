1ac4a247b7bea91a4f07ecd128c4922e999b4c1f
laurajimu <laura.jimenez@hackerdreams.org>
2025-04-08 10:58:33 +0000

    cambio

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,8 @@ function App() {
                 <Link className='links px-4 font-weight-bold text-white' to="/Sponsor#patrocinio"  style={{ marginTop: '10px', marginBottom: '10px', textDecoration: 'none' }}>CONVIERTETE EN PATROCINADOR</Link>
                 <Link className='links px-4 font-weight-bold text-white' to="/Organizer#organizr" style={{ marginTop: '10px', marginBottom: '10px', textDecoration: 'none' }}>ORGANIZADORES</Link>
                 <NavDropdown title="EVENTOS ANTERIORES" id="basic-nav-dropdown" className="px-4 font-weight-bold text-white">
-                  <NavDropdown.Item as={Link} to="/#Events2024">XOPS 2024</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/archive/2023/Events2024">XOPS 2024</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/archive/2023/Organizer2024">XOPS 2024</NavDropdown.Item>
                   <NavDropdown.Item as={Link} to="/archive/2023/Events2023">XOPS 2023</NavDropdown.Item>
                 </NavDropdown>       
                 {/*Añado el menú EVENTOS ANTERIORES Y LOS SUBMENÚS XOPS 2034 Y 2023 */}                                             
@@ -75,7 +76,7 @@ function App() {
           <Route path="/Home2023" element={<Home2023 />} />
           <Route path="/Organizer2023" element={<Organizer2023 />} /> {/* Organizador 2023 */}
           <Route path="/archive/2023/Events2023" element={<Events2023 />} /> {/* Eventos 2023 */}
-          <Route path="/Events2024" element={<Events2024 />} /> {/* Eventos 2023 */}
+          <Route path="/archive/2023/Events2023" element={<Events2024 />} /> {/* Eventos 2023 */}
         </Routes>
 
 
