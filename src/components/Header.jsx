fbbab514f1842ba585055cf5e8e8ce20980c500c
laurajimu <laura.jimenez@hackerdreams.org>
2025-04-08 13:06:35 +0000

    agrego scroll en Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,6 +31,14 @@ const Header = () => {
         }
     };
 
+//defino la constante de Eventos Anteriores
+    const scrollToEventosAnteriores = () => {
+        const element = document.getElementById('eventos-anteriores');  // ID de la sección de eventos anteriores
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      };
+
 
     return (
         <Router>
@@ -46,23 +54,8 @@ const Header = () => {
                 <Nav.Link className='links px-4 font-weight-bold text-white' href="#tickets"></Nav.Link>
                 
                 {/* Agrego el NavDropdown para "Eventos Anteriores" */}
-                <NavDropdown title="EVENTOS ANTERIORES" id="basic-nav-dropdown" className="px-4 font-weight-bold text-white">
-                        {/* Submenú para XOPS 2024 */}
-                        <NavDropdown.Item as={Link} to="/archive/2024/Events2024" className="text-white">
-                            XOPS 2024
-                        </NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/archive/2024/Organizer2024" className="text-white">
-                            Organizadores 2024
-                        </NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        {/* Submenú para XOPS 2023 */}
-                        <NavDropdown.Item as={Link} to="/archive/2023/Events2023" className="text-white">
-                            XOPS 2023
-                        </NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/archive/2023/Organizer2023" className="text-white">
-                            Organizadores 2023
-                        </NavDropdown.Item>
-                    </NavDropdown>
+                <NavDropdown onClick={scrollToEventosAnteriores} className='links px-4 font-weight-bold text-white ' href="#vuélvete-patrocinador">EVENTOS ANTERIORES</NavDropdown>
+  
             </Nav>
         <a href="https://www.eventbrite.es/e/entradas-xops-conference-1049115200807?aff=oddtdtcreator" class="button menu-btn">
             Tickets
