ab8b6f33b35d9ec6cd8c231041101e4ec6265ec4
laurajimu <laura.jimenez@hackerdreams.org>
2025-05-09 09:48:18 +0000

    instalacion de reactt helmet provider y modif de archivos main, hero, speakerssection y organizers

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { HelmetProvider } from 'react-helmet-async'; // ya instale reac HelmetProvider
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+//nuevo despues de HelmetProvider
+
+  ReactDOM.createRoot(document.getElementById('root')).render(      
+          <React.StrictMode>
+            <HelmetProvider>
+              <BrowserRouter> { /* O el router que uses */ }
+                <App />
+              </BrowserRouter>
+            </HelmetProvider>
+          </React.StrictMode>
+        );
