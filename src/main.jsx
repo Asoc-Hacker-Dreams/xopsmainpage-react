77f0de5dc867c9a24dbb1212bcd06582b37a7edd
laurajimu <laura.jimenez@hackerdreams.org>
2025-05-09 11:16:32 +0000

    cambios direccion

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,15 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { HelmetProvider } from 'react-helmet-async'; // ya instale reac HelmetProvider
-
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
-//nuevo despues de HelmetProvider
-
-  ReactDOM.createRoot(document.getElementById('root')).render(      
-          <React.StrictMode>
-            <HelmetProvider>
-              <BrowserRouter> { /* O el router que uses */ }
-                <App />
-              </BrowserRouter>
-            </HelmetProvider>
-          </React.StrictMode>
-        );
+  <React.StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
+  </React.StrictMode>
+);
