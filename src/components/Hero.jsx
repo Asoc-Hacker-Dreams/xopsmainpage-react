a51b056edabcb0d4561cdd5a251e348e5ddf0d63
Francisco Arencibia <francisco.arencibia@jotelulu.com>
2025-10-04 20:31:10 +0200

    improve mob version

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,10 +20,10 @@ import { Helmet } from 'react-helmet-async';  // añado despues de instalar reac
 {/* function Hero() {
   return (   */}
     <div className='Hero-section d-flex align-items-center justify-content-center text-center'>
-      <div className="d-flex align-items-center justify-content-center text-center text-white py-5">
+      <div className="justify-content-center text-center text-white py-5">
     <div className="container">
-        <h1 className="display-4 font-weight-bold">¡ÚNETE A LA REVOLUCIÓN X-OPS!    </h1>
-        <p className="lead">El mundo de las IT está cambiando. Únete a nosotros en la X-Ops Conference, donde descubrirás cómo la tecnología y las personas adecuadas están impulsando el cambio.        </p>
+        <h1 className="display-4 font-weight-bold">¡ÚNETE A LA REVOLUCIÓN <span className="xops-title">X-OPS!</span></h1>
+        <p className="lead hero-lead-text">El mundo de las IT está cambiando. Únete a nosotros en la X-Ops Conference, donde descubrirás cómo la tecnología y las personas adecuadas están impulsando el cambio.</p>
         <p className="lead">Fecha: 21 y 22 de Noviembre 2025 </p>
         <div className="mt-4 mx-4">
         <a href="https://www.eventbrite.ch/e/entradas-x-ops-conference-madrid-2025-1306767269079" className="btn mx-2 my-2 bg-color text-white btn-lg mr-3">Compra tu entrada</a>
