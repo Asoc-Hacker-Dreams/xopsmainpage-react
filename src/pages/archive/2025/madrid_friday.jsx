9a3b4407e62d6071c525a21572786a37767cb5c5
Francisco Arencibia <francisco.arencibia@jotelulu.com>
2025-10-25 22:34:34 +0200

    fix agenda

--- src/pages/archive/2025/madrid_friday.jsx
+++ src/pages/archive/2025/madrid_friday.jsx
@@ -45,7 +45,7 @@ const MadridFriday = () => {
                 <div className="overlay"></div>
                 <div className="card-body text-white">
                   <h5 className="card-title"><span className='heading'>Inicio: </span>Salón de Actos</h5>
-                  <p className="card-text">9:00 h</p>
+                  <p className="card-text">9:30 h</p>
                   <p>Apertura de puertas.</p>
                 </div>
               </div>
